a486cd1a046ec190c62f69dfaecf5ebcf32e2ee5
Salman Shaikh <mshaik52@uwo.ca>
2020-07-26 18:46:02 -0400

    intital commit

---
+++ src/test/java/guru/framework/spring5webapp/Spring5webappApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.framework.spring5webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5webappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
